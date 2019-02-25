e1b42d653333bd806c84f640f70213836f8cfad6
Andre Ferreira <andrehrf@gmail.com>
2019-02-25 15:55:22 -0300

    include promise

--- src/index.js
+++ src/index.js
@@ -2,55 +2,60 @@ import { $ } from "@dekproject/scope";
 import Redis from "ioredis";
 
 export default async () => {
-    try{
-        let redisConfig = {};
-        let env = process.env;
-        let configApproved = true;
+    return new Promise(async (resolve, reject) => {
+        try{
+            let redisConfig = {};
+            let env = process.env;
+            let configApproved = true;
 
-        // Check the existence of the parameters below in .env
+            // Check the existence of the parameters below in .env
 
-        if(env.hasOwnProperty('REDIS_HOST') && !!env.REDIS_HOST)
-            redisConfig['host'] = env.REDIS_HOST;
-        else {
-            configApproved = false;
-            console.log('[ Redis Plugin ] - There is no REDIS_HOST variable in the .env file');
-        }
-
-        if(env.hasOwnProperty('REDIS_PORT') && !!env.REDIS_PORT)
-            redisConfig['port'] = env.REDIS_PORT;
-        else {
-            configApproved = false;
-            console.log('[ Redis Plugin ] - There is no REDIS_PORT variable in the .env file');
-        }
-
-        if(env.hasOwnProperty('REDIS_FAMILY') && !!env.REDIS_FAMILY)
-            redisConfig['family'] = env.REDIS_FAMILY;
-
-        if(env.hasOwnProperty('REDIS_PASSWORD') && !!env.REDIS_PASSWORD)
-            redisConfig['password'] = env.REDIS_PASSWORD;
-
-        if(env.hasOwnProperty('REDIS_DB') && !!env.REDIS_DB)
-            redisConfig['db'] = env.REDIS_DB;
-
-        if(!configApproved){
-            console.log('[ Redis Plugin ] - Please correct the above errors before restarting the application.')
-            process.exit(-1);
-        }
-        else {
-            try {
-                let redis = new Redis(redisConfig);
-
-                if(env.PLUGIN_DEBUG == 'true')
-                    console.log(`[ Redis Plugin ] - Successfully signed`);
-
-                $.set("redis", redis);
+            if(env.hasOwnProperty('REDIS_HOST') && !!env.REDIS_HOST)
+                redisConfig['host'] = env.REDIS_HOST;
+            else {
+                configApproved = false;
+                console.log('[ Redis Plugin ] - There is no REDIS_HOST variable in the .env file');
             }
-            catch (e) {
-                console.log(`[ Redis Plugin ] - ${e.message}`)
+
+            if(env.hasOwnProperty('REDIS_PORT') && !!env.REDIS_PORT)
+                redisConfig['port'] = env.REDIS_PORT;
+            else {
+                configApproved = false;
+                console.log('[ Redis Plugin ] - There is no REDIS_PORT variable in the .env file');
+            }
+
+            if(env.hasOwnProperty('REDIS_FAMILY') && !!env.REDIS_FAMILY)
+                redisConfig['family'] = env.REDIS_FAMILY;
+
+            if(env.hasOwnProperty('REDIS_PASSWORD') && !!env.REDIS_PASSWORD)
+                redisConfig['password'] = env.REDIS_PASSWORD;
+
+            if(env.hasOwnProperty('REDIS_DB') && !!env.REDIS_DB)
+                redisConfig['db'] = env.REDIS_DB;
+
+            if(!configApproved){
+                console.log('[ Redis Plugin ] - Please correct the above errors before restarting the application.')
+                process.exit(-1);
+            }
+            else {
+                try {
+                    let redis = new Redis(redisConfig);
+
+                    if(env.PLUGIN_DEBUG == 'true')
+                        console.log(`[ Redis Plugin ] - Successfully signed`);
+
+                    $.set("redis", redis);
+                    resolve();
+                }
+                catch (e) {
+                    console.log(`[ Redis Plugin ] - ${e.message}`);
+                    reject();
+                }
             }
         }
-    }
-    catch(e){
-        console.log(`[ Redis Plugin ] - ${e.message}`)
-    }
+        catch(e){
+            console.log(`[ Redis Plugin ] - ${e.message}`);
+            reject();
+        }
+    });
 }
