c49333c1600671f8137e81552b3b036a485911d6
Andre Ferreira <andrehrf@gmail.com>
2019-02-25 19:10:36 -0300

    include wait function

--- sample/index.js
+++ sample/index.js
@@ -5,8 +5,10 @@ import { $, plugins } from "@dekproject/scope";
     dotenv.config({ path: "./sample/.env" });
     await plugins("./");
 
-    $.redis.multi().set("foo", "bar").get("foo").exec(function (err, results) {
-        if(err) console.log(err);
-        else console.log(results);
+    $.wait("redis").then(() => {
+        $.redis.multi().set("foo", "bar").get("foo").exec(function (err, results) {
+            if(err) console.log(err);
+            else console.log(results);
+        });
     });
 })();
